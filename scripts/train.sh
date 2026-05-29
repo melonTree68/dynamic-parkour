44032e77c36ec9e8875573b92a5071a42f6bb766
Zhijie Chen <zhijie.chen2007@outlook.com>
2026-05-29 19:31:48 +0000

    chore: add a training command

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,6 +3,7 @@ source ~/extreme-parkour/scripts/rc.sh
 
 # train base policy
 python train.py --no_wandb --headless --task a1_dynamic --proj_name original-pipeline-dynamic-terrain --exptid base
+python train.py --no_wandb --headless --resume --resumeid base-expert --checkpoint 15000 --task a1_dynamic --proj_name imitation-pretrain-dynamic-terrain --exptid resume-from-base-15k
 
 # train distillation policy
 python train.py --no_wandb --delay --use_camera --resume --resumeid base --checkpoint 15000 --task a1_dynamic --proj_name original-pipeline-dynamic-terrain --exptid distill-from-15k
