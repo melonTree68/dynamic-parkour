37d09f36dfada22efd76aea6da8f3fa35441c40d
Zhijie Chen <zhijie.chen2007@outlook.com>
2026-06-10 12:51:19 +0800

    chore: modify train script

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,4 +6,4 @@ python train.py --no_wandb --headless --task a1_dynamic --proj_name original-pip
 python train.py --no_wandb --headless --resume --resumeid base-expert --checkpoint 15000 --task a1_dynamic --proj_name imitation-pretrain-dynamic-terrain --exptid resume-from-base-15k
 
 # train distillation policy
-python train.py --no_wandb --delay --use_camera --resume --resumeid base --checkpoint 15000 --task a1_dynamic --proj_name original-pipeline-dynamic-terrain --exptid distill-from-15k
+python train.py --no_wandb --delay --use_camera --resume --resumeid resume-from-imitate-base-15k-100-v2-91bf8ce --checkpoint 15000 --task a1_dynamic --proj_name imitation-pretrain-dynamic-terrain --exptid distill-from-resume-from-imitate-base-15k-100-15k-v2-91bf8ce
