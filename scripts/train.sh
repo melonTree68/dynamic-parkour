fba166268d14fa0162656cafefbbfec618e908dc
Zhijie Chen <zhijie.chen2007@outlook.com>
2026-06-14 07:42:05 +0000

    chore: update train script

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,5 +6,7 @@ python train.py --no_wandb --headless --task a1_dynamic --proj_name original-pip
 python train.py --no_wandb --headless --resume --resumeid base-expert --checkpoint 15000 --task a1_dynamic --proj_name imitation-pretrain-dynamic-terrain --exptid resume-from-base-15k
 
 # train distillation policy
-python train.py --no_wandb --delay --use_camera --resume --resumeid resume-from-imitate-base-15k-100 --checkpoint 15000 --task a1_dynamic --proj_name augment-latent-roa-dynamic-terrain --exptid distill-from-resume-from-imitate-base-15k-100-15k
-python train.py --no_wandb --delay --use_camera --resume --resumeid resume-from-imitate-base-15k-100 --checkpoint 15000 --task a1_dynamic --proj_name augment-latent-ts-dynamic-terrain --exptid distill-from-resume-from-imitate-base-15k-100-15k
+# a1 dynamic
+python train.py --no_wandb --delay --use_camera --resume --resumeid resume-from-imitate-base-15k-100 --checkpoint 20000 --task a1_dynamic --proj_name augment-latent-roa-dynamic-terrain --exptid distill-from-resume-from-imitate-base-15k-100-20k
+# a1 mixed
+python train.py --no_wandb --delay --use_camera --resume --resumeid resume-from-imitate-base-15k-100 --checkpoint 20000 --task a1_mixed --proj_name augment-latent-hybrid-mixed-terrain --exptid distill-from-resume-from-imitate-base-15k-100-20k
